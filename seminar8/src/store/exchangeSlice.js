e38ae204dd16a40a7b8e11838ca5d68500abd5df
Dmitrii <aleksdima@mail.ru>
2024-10-14 21:28:24 +0300

    seminar 8 fix some bug

--- seminar8/src/store/exchangeSlice.js
+++ seminar8/src/store/exchangeSlice.js
@@ -12,17 +12,21 @@ const initialState = {
 export const fetchExchange = createAsyncThunk('exchange/fetchExchange',
     async (_, thunkApi) => {              // {rejectWithValue,getState,dispatch}=thunkApi функции внутри thunkApi
         try {
-            const { convertation } = thunkApi.getState();
+            const { exchange } = thunkApi.getState();
             const response = await fetch("https://v6.exchangerate-api.com/v6/276b850ba7a1c710fc835a20/latest/RUB");
             if (!response.ok) {
                 throw new Error({ message: response.status });
             }
             const data = await response.json();
-            if (!convertation.currencyList.length) {
-                thunkApi.dispatch(setCurrencyList(Object.keys(data.convertation_rates)));
-                console.log(Object.keys(data.convertation_rates))
+            console.log(exchange)
+            if (!exchange.currencyList.length) {
+                thunkApi.dispatch(setCurrencyList(Object.keys(data.conversion_rates
+                )));
+                console.log(Object.keys(data.conversion_rates
+                ))
             }
-            return data.convertation_rates  //попадет в payload 
+            return data.conversion_rates
+            //попадет в payload 
         } catch (error) {
             return thunkApi.rejectWithValue(error.message)
         }
